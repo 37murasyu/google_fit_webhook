4becfd504dc593bf8fffd2ce3ad1cb983c9c3e81
Shuta Murayama <murayama.s.aa@m.titech.ac.jp>
2025-05-13 02:34:39 +0900

    Add root route for homepage check

--- app.py
+++ app.py
@@ -2,6 +2,9 @@ from flask import Flask
 import subprocess
 
 app = Flask(__name__)
+@app.route("/")
+def index():
+    return "Google Fit Webhook is running!"
 
 @app.route("/wake_alert")
 def wake_alert():
